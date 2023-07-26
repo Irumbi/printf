798f97b59b3e38fe4a124487a609428192188265
John  Irumbi <irumbij@gmail.com>
2023-07-25 18:38:48 -0700

    Printf

--- main.h
+++ main.h
@@ -1,9 +1,12 @@
+main.h
+
+
+
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
