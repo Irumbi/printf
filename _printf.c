ac3ec5a7a518f01b6b5528edf7bc51e40b6b8201
John  Irumbi <irumbij@gmail.com>
2023-07-25 18:56:35 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,67 +1,86 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ * Return: On success, 1. On error, -1 is returned.
  */
-int _printf(const char *format, ...)
+int _putchar(char c)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+	return (write(1, &c, 1));
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * _printf - Custom printf function
+ * @format: Format string containing the directives
+ * Return: The number of characters printed (excluding the null byte)
  */
-void print_buffer(char buffer[], int *buff_ind)
+int _printf(const char *format, ...)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+	va_list args;
+	int count = 0;
 
-	*buff_ind = 0;
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			count++;
+		}
+		else
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				_putchar(va_arg(args, int));
+				count++;
+				break;
+			case 's':
+				count += _print_str(va_arg(args, char *));
+				break;
+			case '%':
+				_putchar('%');
+				count++;
+				break;
+			default:
+				_putchar('%');
+				_putchar(*format);
+				count += 2;
+				break;
+			}
+		}
+		format++;
+	}
+
+	va_end(args);
+
+	return (count);
+	}
+
+	/**
+	* _print_str - Prints a string to stdout
+	* @str: The string to print
+	* Return: The number of characters printed
+	*/
+	int _print_str(char *str)
+	{
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str)
+	{
+		_putchar(*str);
+		count++;
+		str++;
+	}
+
+	return (count);
 }
 
